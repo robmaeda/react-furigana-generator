6e801335343f280ccb984cb14c91f67095f086e4
Robert Maeda <rob.maeda3@gmail.com>
2024-08-26 22:22:24 -0700

    Update font and styling

--- src/App.tsx
+++ src/App.tsx
@@ -36,33 +36,34 @@ const App = () => {
   const disabled = isLoading || !inputText.trim();
 
   return (
-    <div className="px-10 py-5">
-      <div className="text-2xl">Furigana Generator</div>
-      <div className="flex mt-4">
-        <div className="flex flex-col">
+    <div className="px-10 py-5 flex flex-col items-center">
+      <div className="text-3xl">Furigana Generator</div>
+      <div className="mt-2">Input Japanese text to get its furigana instanty with our generator.</div>
+      <div className="flex flex-col items-center justify-center mt-14">
+        <div className="flex">
           <textarea
             value={inputText}
             onChange={handleInputChange}
             placeholder="Enter Japanese text here"
-            className="w-96 h-36 p-2 border resize-none border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className="w-96 h-48 p-2 border resize-none border-[#1E1E1E] focus:outline-none focus:ring-2 bg-[#EDE8DF] focus:border-transparent"
           />
-          <button
-            onClick={handleButtonClick}
-            disabled={disabled}
-            className={`mt-2 w-48 px-4 py-2 bg-blue-500 text-white rounded-md ${
-              disabled ? "!bg-blue-300 cursor-not-allowed" : "hover:bg-blue-700"
-            }`}
-          >
-            Generate Furigana
-          </button>
-        </div>
-        <div className="w-96 h-36 ml-10 border border-gray-300 bg-gray-100 p-2 rounded-md">
+          <div className="w-96 h-48 ml-10 border border-[#1E1E1E] bg-[#E6E0D5] p-2">
           {isLoading ? (
             "Loading..."
           ) : (
             <div>{parse(outputText)}</div>
           )}
+          </div>
         </div>
+        <button
+            onClick={handleButtonClick}
+            disabled={disabled}
+            className={`mt-16 w-64 px-4 py-2 bg-[#8C4843] text-white ${
+              disabled ? "cursor-not-allowed opacity-60" : ""
+            }`}
+          >
+            Generate
+        </button>
       </div>
     </div>
   );
