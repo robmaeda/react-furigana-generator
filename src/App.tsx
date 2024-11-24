04c464b5c96d861c849ee83fce2c2a5ffb56220b
Robert Maeda <rob.maeda3@gmail.com>
2024-11-23 21:05:57 -0800

    Add hiragana only to output

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import axios from "axios";
 import parse from "html-react-parser";
 import { useState } from "react";
 
+interface OutputText {
+  furigana: string;
+  hiragana: string;
+}
+
 const App = () => {
   const [inputText, setInputText] = useState<string>("");
-  const [outputText, setOutputText] = useState<string>("");
+  const [outputText, setOutputText] = useState<OutputText>({furigana: "", hiragana: ""});
+  const [showFurigana, setShowFurigana] = useState(true);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -27,7 +33,7 @@ const App = () => {
       setOutputText(response.data.result);
     } catch (error) {
       console.error("Error fetching Furigana:", error);
-      setOutputText("Failed to fetch Furigana.");
+      setOutputText({ furigana: "Failed to fetch Furigana. Please try again", hiragana: "" });
     } finally {
       setIsLoading(false);
     }
@@ -38,7 +44,7 @@ const App = () => {
   return (
     <div className="px-10 py-5 flex flex-col items-center xl:pt-14">
       <div className="text-3xl">Furigana Generator</div>
-      <div className="mt-2">Input Japanese text to get its furigana instanty with our generator.</div>
+      <div className="mt-2">Input Japanese text to get its furigana instantly with our generator.</div>
       <div className="mt-4">Paste the example below: 仕事を探していますよ。</div>
       <div className="flex flex-col items-center justify-center mt-14">
         <div className="flex">
@@ -52,10 +58,11 @@ const App = () => {
           {isLoading ? (
             "Loading..."
           ) : (
-            <div>{parse(outputText)}</div>
+            <div>{showFurigana ? parse(outputText.furigana) : outputText.hiragana}</div>
           )}
           </div>
         </div>
+        {outputText.furigana && <button className="mt-16 w-64 px-4 py-2 bg-[#8C4843] text-white" onClick={() => setShowFurigana((prevState) => !prevState)}>{showFurigana ? "Show hiragana only" : "Show furigana"}</button>}
         <button
             onClick={handleButtonClick}
             disabled={disabled}
